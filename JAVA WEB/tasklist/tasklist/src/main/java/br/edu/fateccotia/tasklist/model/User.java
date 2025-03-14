cf97f1e16d3d081ed157b179df5a42ae88d3a17a
unknown <rufinoguilherme633@gmail.com>
2025-03-13 22:22:02 -0300

    ligação User and Task

--- JAVA WEB/tasklist/tasklist/src/main/java/br/edu/fateccotia/tasklist/model/User.java
+++ JAVA WEB/tasklist/tasklist/src/main/java/br/edu/fateccotia/tasklist/model/User.java
@@ -20,5 +20,37 @@ public class User {
 	public User() {
 		// TODO Auto-generated constructor stub
 	}
+	public User(Integer id, String username, String password, String email) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.email = email;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
 	
 }
