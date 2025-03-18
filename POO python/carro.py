b751e9135320d2e7e8373992c4d2f8e9fd5d7542
unknown <rufinoguilherme633@gmail.com>
2025-03-17 21:42:07 -0300

    POO nova aula - python

---
+++ POO python/carro.py
@@ -0,0 +1,39 @@
+class Carro:
+    def __init__(self,marca,modelo):
+        self.marca = marca
+        self.modelo = modelo
+
+    def exibirInfo(self):
+        return f"{self.marca} {self.modelo}"
+    
+carro = Carro("HB20","Hyundai")
+
+print(carro.exibirInfo())
+
+
+
+class Pessoa:
+    def __init__(self,nome,idade):
+        self.nome = nome
+        self.idade = idade
+    
+    def apresentar(self):
+        return f"{self.nome} {self.idade}"
+    
+
+pessoa1 = Pessoa("Guilherme","Rufino");
+print(pessoa1.apresentar())
+
+
+
+class Estudante(Pessoa):
+    def __init__(self,nome,idade,matricula):
+        super().__init__(nome,idade)
+        self.matricula = matricula
+    def apresentar(self):
+        mensagem_pai = super().apresentar();
+        return f"{mensagem_pai} matricula: {self.matricula}"
+    
+
+estudante = Estudante("Guilherme", "Campos",22222)
+print(estudante.apresentar())
